b09f93060a84908e6399f1bac473e93380a82210
yagita <yagita.takashi@gmail.com>
2021-11-27 13:04:28 +0900

    Initial commit

---
+++ tests/unit/test_cdk_ssm_parameter_store_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_ssm_parameter_store.cdk_ssm_parameter_store_stack import CdkSsmParameterStoreStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_ssm_parameter_store/cdk_ssm_parameter_store_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkSsmParameterStoreStack(app, "cdk-ssm-parameter-store")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
