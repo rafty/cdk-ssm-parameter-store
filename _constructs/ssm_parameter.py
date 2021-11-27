7696431ec2356af5363cab0433c13717d6748ec2
yagita <yagita.takashi@gmail.com>
2021-11-27 16:07:18 +0900

    refactors

--- _constructs/ssm_parameter.py
+++ _constructs/ssm_parameter.py
@@ -11,17 +11,13 @@ class SsmStringParameter(cdk.Construct):
                  ) -> None:
         super().__init__(scope, id)
 
-        email_parameter = aws_ssm.StringParameter(
+        aws_ssm.StringParameter(
             scope=self,
             id=f'{id}-construct',
-            # parameter_name='/my-site/alerts-email-dev',
             parameter_name=parameter_name,
-            # string_value='hoge-dev@example.com',
             string_value=string_value,
-            # description='alert on dev to the email',
             type=aws_ssm.ParameterType.STRING,
             tier=aws_ssm.ParameterTier.STANDARD,
-            # allowed_pattern='.*'
         )
 
 
@@ -37,9 +33,7 @@ class StringListParameter(cdk.Construct):
         aws_ssm.StringListParameter(
             scope=self,
             id=f'{id}-construct',
-            # parameter_name='/my-site/environments',
             parameter_name=parameter_name,
-            # string_list_value=list(('dev', 'staging', 'prod')),
             string_list_value=string_list_value,
             tier=aws_ssm.ParameterTier.ADVANCED
         )
