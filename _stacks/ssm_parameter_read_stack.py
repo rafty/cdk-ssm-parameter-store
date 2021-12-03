f755c383901d7b40d2856798c708998d36ed5834
yagita <yagita.takashi@gmail.com>
2021-12-03 16:52:54 +0900

    Update CDK v2

--- _stacks/ssm_parameter_read_stack.py
+++ _stacks/ssm_parameter_read_stack.py
@@ -1,10 +1,11 @@
 import json
-import aws_cdk as cdk
+from aws_cdk import Stack
+from aws_cdk import CfnOutput
 from constructs import Construct
 from aws_cdk import aws_ssm
 
 
-class TestReadParameterStack(cdk.Stack):
+class TestReadParameterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -14,7 +15,7 @@ class TestReadParameterStack(cdk.Stack):
             parameter_name='/my-site/alerts-email-dev'
         )
 
-        cdk.CfnOutput(
+        CfnOutput(
             scope=self,
             id='parameter-email',
             value=email
@@ -26,7 +27,7 @@ class TestReadParameterStack(cdk.Stack):
             string_list_parameter_name='/my-site/environments',
         )
 
-        cdk.CfnOutput(
+        CfnOutput(
             scope=self,
             id='parameter_environments',
             value=json.dumps(self.resolve(environments.string_list_value))
